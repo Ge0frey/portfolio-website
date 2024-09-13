df2e495d9c426c35b893c48502e26786e3d4d91a
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-13 05:35:11 +0300

    Header scroll effect.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// new function for nav transition
+window.addEventListener('scroll', () => {
+  const nav = document.querySelector('nav');
+  if (window.scrollY > 50) {
+    nav.classList.add('nav-scrolled');
+  } else {
+    nav.classList.remove('nav-scrolled');
   }
-  
+});
